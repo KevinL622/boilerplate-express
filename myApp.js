903099c7642f3ae544fbb2f1122fae3bb1c10674
kevinl622@gmail.com <kevinl622@gmail.com>
2018-09-05 18:06:55 -0700

    Added Solution to Challenge Ten

--- myApp.js
+++ myApp.js
@@ -59,7 +59,11 @@ app.get('/:word/echo', function(req, res){
 
 /** 10) Get input from client - Query parameters */
 // /name?first=<firstname>&last=<lastname>
-
+app.route('/name').get(function(req, res){
+  var firstname = req.query.first;
+  var lastname = req.query.last;
+  res.json({"name": firstname + " " + lastname});
+});
 
 /** 11) Get ready for POST Requests - the `body-parser` */
 // place it before all the routes !
